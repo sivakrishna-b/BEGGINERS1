b7a6acf976c36f7039891efc299b65ff8f3b946f
sivakrishna <42761102+sivakrishna-b@users.noreply.github.com>
2018-09-06 09:30:46 +0530

    Update evenorodd.py

--- evenorodd.py
+++ evenorodd.py
@@ -1,5 +1,5 @@
-num = int(input("Enter a number: "))
-if (num % 2) == 0:
-   print("{0} is Even".format(num))
+x = int(input("Enter a number: "))
+if (x % 2) == 0:
+   print("{0} is Even".format(x))
 else:
-   print("{0} is Odd".format(num))
+   print("{0} is Odd".format(x))
