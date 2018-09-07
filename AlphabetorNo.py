5726a8bf385ee1e873ccb2ea078135b33c41df08
sivakrishna <42761102+sivakrishna-b@users.noreply.github.com>
2018-09-07 09:41:49 +0530

    Create AlphabetorNo.py

---
+++ AlphabetorNo.py
@@ -0,0 +1,6 @@
+sd = raw_input()
+karthi = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M']
+if (sd in karthi):
+	print("Alphabet")
+else:
+	print("No")
